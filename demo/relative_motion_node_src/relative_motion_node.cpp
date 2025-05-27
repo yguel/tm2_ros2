1af50b8afe861618b6f6ef691697f339a0658421
Manuel YGUEL <yguel@unistra.fr>
2025-05-27 16:57:14 +0200

    WIP compile, add node creating service to GoToRelativePosition

---
+++ demo/relative_motion_node_src/relative_motion_node.cpp
@@ -0,0 +1,146 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "tm_msgs/srv/send_script.hpp"
+#include "tm_msgs/msg/svr_response.hpp"
+#include "tm_msgs/srv/go_to_relative_position.hpp"
+
+#include <chrono>
+#include <cstdlib>
+
+#include <sstream>
+#include <string>
+
+
+using namespace std::chrono_literals;
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+class RelativeMotionNode : public rclcpp::Node
+{
+public:
+  RelativeMotionNode()
+  : Node("Omron_relative_motion")
+  {
+    m_send_script_client = this->create_client<tm_msgs::srv::SendScript>("send_script");
+    if (!m_send_script_client->wait_for_service(1s)) {
+      RCLCPP_ERROR_STREAM(
+        rclcpp::get_logger("rclcpp"), "Service 'send_script' not available, exiting.");
+      rclcpp::shutdown();
+      return;
+    }
+
+    m_svr_response_subscription = this->create_subscription<tm_msgs::msg::SvrResponse>(
+      "svr_response", 10, std::bind(&RelativeMotionNode::svr_response_callback, this, _1));
+
+    // Create service server for relative motion commands
+    m_relative_motion_service = this->create_service<tm_msgs::srv::GoToRelativePosition>(
+      "go_to_relative_position",
+      std::bind(&RelativeMotionNode::command_callback, this, _1, _2));
+  }
+
+  bool send_cmd(const std::string & cmd, const std::string & id)
+  {
+    auto request = std::make_shared<tm_msgs::srv::SendScript::Request>();
+    request->id = id;
+    request->script = cmd;
+
+    while (!m_send_script_client->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR_STREAM(
+          rclcpp::get_logger(
+            "rclcpp"), "Interrupted while waiting for the service. Exiting.");
+        return false;
+      }
+      RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    }
+
+    auto result = m_send_script_client->async_send_request(request);
+    auto node_shared = this->shared_from_this();
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(node_shared, result) ==
+      rclcpp::FutureReturnCode::SUCCESS)
+    {
+      std::string msg = id + std::string(" ") + cmd;
+      if (result.get()->ok) {
+        RCLCPP_INFO_STREAM(
+          rclcpp::get_logger("rclcpp"), (std::string(
+            "Relative motion OK: ") + msg).c_str());
+        return true;
+      } else {
+        RCLCPP_INFO_STREAM(
+          rclcpp::get_logger("rclcpp"), (std::string(
+            "Relative motion failed: ") + msg).c_str());
+        return false;
+      }
+    } else {
+      RCLCPP_ERROR_STREAM(
+        rclcpp::get_logger("rclcpp"),
+        (std::string("Failed to call service for relative motion: ") + cmd).c_str());
+      return false;
+    }
+    return false;
+  }
+
+protected:
+  void svr_response_callback(const tm_msgs::msg::SvrResponse::SharedPtr msg) const
+  {
+
+    RCLCPP_INFO_STREAM(
+      this->get_logger(),
+      "SvrResponse: id is = " << msg->id << ", mode is " << (int)msg->mode << ", content is " << msg->content << ", error code is " <<
+        (int)msg->error_code);
+  }
+
+  void command_callback(
+    const std::shared_ptr<tm_msgs::srv::GoToRelativePosition::Request> request,
+    std::shared_ptr<tm_msgs::srv::GoToRelativePosition::Response> response)
+  {
+    // For command format see Software_Expression - Editor - and - Listen - Node_1 .84_Rev1 .00_EN.pdf page 252
+
+    std::ostringstream ss;
+    // C: expressed in current base, P: Speed format as a percentage, P: blending format as a percentage
+    ss << "Move_PTP(\"CPP\",";
+    // relative motion parameters
+    ss << request->x << "," << request->y << "," << request->z << ","
+       << request->rx_deg << "," << request->ry_deg << "," << request->rz_deg << ",";
+    // speed percentage
+    ss << request->speed_percent << ", ";
+    // blending percentage
+    ss << "200, ";
+    // Disable precise positioning
+    ss << "false)";
+
+    // Log the command
+    const std::string cmd = ss.str();
+    RCLCPP_INFO_STREAM(
+      this->get_logger(),
+      "Relative motion command: " << ss.str());
+
+    // Setup the ID for the command
+    std::ostringstream id_ss;
+    id_ss << "RelativeGoTo_" << m_cmd_id_counter;
+
+    response->goal_reached = send_cmd(cmd, id_ss.str());
+    m_cmd_id_counter++;
+  }
+
+protected:
+  rclcpp::Subscription<tm_msgs::msg::SvrResponse>::SharedPtr m_svr_response_subscription;
+
+  rclcpp::Service<tm_msgs::srv::GoToRelativePosition>::SharedPtr m_relative_motion_service;
+
+  rclcpp::Client<tm_msgs::srv::SendScript>::SharedPtr m_send_script_client;
+
+  unsigned int m_cmd_id_counter = 0;
+
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<RelativeMotionNode>());
+  rclcpp::shutdown();
+  return 0;
+}
